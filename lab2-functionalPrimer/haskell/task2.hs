ed721f81eb603cca26836e762110c37f17eb5654
gb-joel <240119397@aston.ac.uk>
2026-02-03 11:34:44 +0000

    completed task2

--- lab2-functionalPrimer/haskell/task2.hs
+++ lab2-functionalPrimer/haskell/task2.hs
@@ -1,4 +1,9 @@
+main :: IO ()
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    print (onePlusone 1)
+
+onePlusone :: Integer -> Integer
+onePlusone x = x + 1
